6ff422d0acacb5299496eaf8026cb259a282e9a5
langtonfavor <langtonfavor@gmail.com>
2023-08-01 14:36:52 +0200

    modified the code to render both title and body

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,12 +20,8 @@ function App() {
   };
 
   const handleItemClick = (item) => {
-    if (item && item.id) {
-      setSelectedItem(item);
-      setShowModal(true);
-      console.log("selectedItem:", selectedItem);
-      console.log("showModal:", showModal);
-    }
+    setSelectedItem(item);
+    setShowModal(true);
   };
 
   const handleCloseModal = () => {
@@ -37,46 +33,53 @@ function App() {
       <h1 className="text-center text-3xl font-bold mb-4">Item List</h1>
       <div className="row row-cols-1 row-cols-md-3 g-4">
         {items.map((item) => (
-          item && item.id ? (
-            <div
-              key={item.id}
-              className="col"
-              onClick={() => handleItemClick(item)}
-              style={{ cursor: 'pointer' }}
-            >
-              <div className="card h-100">
-                <div className="card-body">
-                  <h3 className="card-title">{item.title}</h3>
+          <div
+            key={item.id}
+            className="col"
+            onClick={() => handleItemClick(item)}
+            style={{ cursor: 'pointer' }}
+          >
+            <div className="card h-100">
+              <div className="card-body">
+                <h3 className="card-title">{item.title}</h3>
+                {/* Show only the body in the modal */}
+                {showModal && selectedItem && selectedItem.id === item.id && (
                   <p className="card-text text-muted">{item.body}</p>
-                </div>
+                )}
               </div>
             </div>
-          ) : null
+          </div>
         ))}
       </div>
 
-      {selectedItem && showModal && (
-  <div className="modal show" tabIndex="-1" role="dialog" style={{ display: 'block' }}>
-    <div className="modal-dialog" role="document">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h5 className="modal-title">{selectedItem.title}</h5>
-          <button type="button" className="close" onClick={handleCloseModal}>
-            <span aria-hidden="true">&times;</span>
-          </button>
+      {/* Bootstrap Modal */}
+      {selectedItem && (
+        <div
+          className={`modal ${showModal ? 'show' : ''}`}
+          tabIndex="-1"
+          role="dialog"
+          style={{ display: showModal ? 'block' : 'none' }}
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">{selectedItem.title}</h5>
+                <button type="button" className="close" onClick={handleCloseModal}>
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <p>{selectedItem.body}</p>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={handleCloseModal}>
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="modal-body">
-          <p>{selectedItem.body}</p>
-        </div>
-        <div className="modal-footer">
-          <button type="button" className="btn btn-secondary" onClick={handleCloseModal}>
-            Close
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
-)}
+      )}
     </div>
   );
 }
